c973920267a56e81fe01d9ecb93587836c8316e6
Rodrigo <rodrigomoraes3040@gmail.com>
2025-10-10 01:30:35 -0300

    refact: adicionado, ao desafio 6, validações de entrada de símbolos aritméticos, números negativos e ou decimais.

--- desafio_006/main.js
+++ desafio_006/main.js
@@ -1,5 +1,23 @@
 const resultado = document.getElementById('resultado')
 
+function evitarSimbolos(evento) {
+    const simbolos = ['-', '+', 'e']
+    if (simbolos.includes(evento.key)) {
+        evento.preventDefault()
+    }
+}
+
+function evitarFloatENegativo (evento) {
+    let input = evento.target
+    let value = input.value
+
+    if (value < 0) {
+        input.value = value * -1
+    } else if (!Number.isInteger(Number(value))) {
+        input.value = parseInt(value)
+    }
+}
+
 function fatorial() {
     let input_value = parseInt(document.getElementById('input_value').value)
     let aux = input_value
@@ -8,6 +26,10 @@ function fatorial() {
         aux = aux * i
     }
 
-    resultado.textContent = `O fatorial de ${input_value} É ${aux}`
+    if (input_value == 0){
+        resultado.textContent = `O fatorial de ${input_value} É ${aux+1}`
+    } else {
+        resultado.textContent = `O fatorial de ${input_value} É ${aux}`
+    }
 
 }
